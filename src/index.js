3c137bc7cfd92615e68ada82c90fcb84fa4ef168
Mailer Martinez <mailermartinez@gmail.com>
2022-09-29 12:25:46 -0500

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(`${__dirname}/data/db.json`);
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
 server.use(router);
+server.use(middlewares)
+
 server.listen(port, () => {
-  console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
