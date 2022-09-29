f2c8d16e4c8fda798b92e2ed5ae94da06d685bb6
Mailer Martinez <mailermartinez@gmail.com>
2022-09-29 12:30:48 -0500

    fixed cors

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
 server.use(router);
 server.use(middlewares)
 
